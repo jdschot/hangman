e1312d5756fad2d50d82aac102dfe2f34b253404
joseppi <joepschot@gmail.com>
2018-03-12 18:51:56 +0100

    added correctGuesses.js as a reducer including a const initialState and export expressions.

---
+++ src/reducers/correctGuesses.js
@@ -0,0 +1,12 @@
+const initialState = {
+  wordToGuess,
+  wordHidden,
+  correctLetters : []
+};
+
+export default function(state = initialState, action = {}) {
+
+  if (action.type === 'CORRECT_GUESS')
+
+  return state
+}
