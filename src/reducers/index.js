ee8ca010a7aacf83c0f114b994a4c0aa02186dc0
joseppi <joepschot@gmail.com>
2018-03-12 19:46:12 +0100

    edited reducers/index.js to include all reducers in the imports and exports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
 import newGame from './newGame'
+import correctGuesses from './correctGuesses'
+import wrongGuesses from './wrongGuesses'
+import showGuesses from './showGuesses'
 
 export default {
   newGame
+  correctGuesses
+  wrongGuesses
+  showGuesses
 }
